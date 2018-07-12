efa5a17c2b4a7755b7c3d9af336ae710d08fb0d7
Charith Mendis <charithm@mit.edu>
2018-07-12 16:20:11 -0400

    lot of DR errors corrected

--- drclients/common/code_embedding.c
+++ drclients/common/code_embedding.c
@@ -1,5 +1,6 @@
 #include "code_embedding.h"
 #include "change_opcode.h"
+#include <string.h>
 
 #define DELIMITER -1
 
@@ -23,6 +24,18 @@ typedef struct {
 } ins_t;
 
 
+
+void print_instr(ins_t * ins){
+
+  dr_printf("ins: ");
+  dr_printf("%s ",ins->name);
+  int i = 0;
+  for(; i < ins->num_ops; i++){
+    dr_printf("%s, ",ins->operands[i].name);
+  }
+  dr_printf("\n");
+
+}
 
 //assumes a cleaned up instruction with only valid mnemonics
 void parse_instr(const char * buffer, int length, ins_t * instr){
@@ -61,10 +74,12 @@ void parse_instr(const char * buffer, int length, ins_t * instr){
     
     if(buffer[i] == '$'){
       instr->operands[op_num].type = IMM_TYPE;
-      while(i < length && buffer[i] != ','){
+      while(i < length && buffer[i] != ',' && buffer[i] != ' '){
 	instr->operands[op_num].name[i - start_operand] = buffer[i];
 	i++;
       }
+      instr->operands[op_num].name[i - start_operand] = '\0';
+      while(i < length && buffer[i] != ',') i++;
     }
     else{ //can be memory or reg
       if(buffer[i] != '%'){ //then it is memory for sure
@@ -78,6 +93,7 @@ void parse_instr(const char * buffer, int length, ins_t * instr){
 	}
 	DR_ASSERT(found_open);
 	instr->operands[op_num].name[i - start_operand] = buffer[i];
+	instr->operands[op_num].name[i - start_operand + 1] = '\0';
 	while(i < length && buffer[i] != ',') i++;
       }
       else{
@@ -99,28 +115,40 @@ void parse_instr(const char * buffer, int length, ins_t * instr){
 	    }
 	    DR_ASSERT(found_open);
 	    instr->operands[op_num].name[i - start_operand] = buffer[i];
+	    instr->operands[op_num].name[i - start_operand + 1] = '\0';
 	    while(i < length && buffer[i] != ',') i++;
 	  }
 	  else if(j < length && buffer[j] == ','){ //no base index
-	    while(i < length && buffer[i] != ','){
+	    while(i < length && buffer[i] != ',' && buffer[i] != ' '){
 	      instr->operands[op_num].name[i - start_operand] = buffer[i];
 	      i++;
 	    }
+	    instr->operands[op_num].name[i - start_operand] = '\0';
+	    while(i < length && buffer[i] != ',') i++;
+	  }
+	  else if(j == length){ //final operand
+	    while(i < length && buffer[i] != ' '){
+	      instr->operands[op_num].name[i - start_operand] = buffer[i];
+	      i++;
+	    }
+	    instr->operands[op_num].name[i - start_operand] = '\0';
+	    while(i < length && buffer[i] != ',') i++;
 	  }
 	}
 	else{
 	  instr->operands[op_num].type = REG_TYPE;
-	  while(i < length && buffer[i] != ','){
+	  while(i < length && buffer[i] != ' ' && buffer[i] != ','){
 	    instr->operands[op_num].name[i - start_operand] = buffer[i];
 	    i++;
 	  }
+	  instr->operands[op_num].name[i - start_operand] = '\0';
+	  while(i < length && buffer[i] != ',') i++;
 	}
       }
 
     }
 
    
-    instr->operands[op_num].name[i - start_operand] = '\0';
     i++;
     while(i < length && buffer[i] == ' ') i++;
     start_operand = i;
@@ -310,6 +338,25 @@ void token_text_embedding(void * drcontext, code_info_t * cinfo, instrlist_t * b
 }
 
 
+
+void textual_embedding(void * drcontext, code_info_t * cinfo, instrlist_t * bb){
+
+  instr_t * instr;
+  int pos = 0;
+  for(instr = instrlist_first(bb); instr != instrlist_last(bb); instr = instr_get_next(instr)){
+
+    if(filter_instr(instr)) continue;
+    
+    pos += instr_disassemble_to_buffer(drcontext,instr,cinfo->code + pos, MAX_CODE_SIZE - 1 -  pos);
+    cinfo->code[pos++] = '\n';
+    DR_ASSERT(pos <= MAX_CODE_SIZE);
+  }
+
+  cinfo->code_size = pos;
+
+}
+
+
 char get_size_prefix(uint32_t size){
   
   switch(size){
@@ -324,41 +371,6 @@ char get_size_prefix(uint32_t size){
 }
 
 
-
-void correct_movs(char * buffer, unsigned start, unsigned end, instr_t * instr){
-
-
-  int opcodes[2] = {OP_movsx, OP_movzx};
-
-  int i = 0;
-  int opcode_num = instr_get_opcode(instr);
-
-  bool found = false;
-  for(i = 0; i < 19; i++){
-    if(opcode_num == opcodes[i]){
-      found = true; break;
-    }
-  }
-
-  if(!found) return;
-
-
-  opnd_t opnd = instr_get_dst(instr, 0);
-  opnd_size_t size = opnd_get_size(opnd);
-  uint32_t size_in_bytes = opnd_size_in_bytes(size);
-
-  char dst_prefix = get_size_prefix(size_in_bytes);
-  
-  opnd = instr_get_src(instr, 0);
-  size = opnd_get_size(opnd);
-  size_in_bytes = opnd_size_in_bytes(size);
-
-  char src_prefix = get_size_prefix(size_in_bytes);
-  
-  if(src_prefix == 'e' || dst_prefix == 'e') return;
-
-
-}
 
 void remove_data(char * buffer, unsigned length){
   
@@ -376,7 +388,7 @@ void remove_data(char * buffer, unsigned length){
   first[4] = '\0';
 
 
-  if(strcmp(first,"data") == 0){
+  if(strcmp(first,"data") == 0 || strcmp(first,"lock") == 0){
 
     while(i < length && buffer[i] == ' ') i++;
 
@@ -395,84 +407,253 @@ void remove_data(char * buffer, unsigned length){
 
 }
 
+void switch_operands(ins_t * ins, int in1, int in2){
 
-void correct_operand_ordering(char * buffer, unsigned start, unsigned end, instr_t * instr){
+
+  char temp[32];
   
-  int opcodes[19] = {OP_cmp,OP_test,OP_ptest,OP_vucomiss,OP_vucomisd,OP_vcomiss,OP_vcomisd,
-                    OP_vptest, OP_vtestps, OP_vtestpd, OP_bound, OP_bt, OP_ucomiss, OP_ucomisd,
-                    OP_comiss, OP_comisd, OP_invept, OP_invvpid, OP_invpcid};
+  int len1 = strlen(ins->operands[in1].name);
+  int len2 = strlen(ins->operands[in2].name);
 
+  strncpy(temp, ins->operands[in1].name, len1);
+  temp[len1] = '\0';
+  strncpy(ins->operands[in1].name, ins->operands[in2].name, len2);
+  ins->operands[in1].name[len2] = '\0';
+  strncpy(ins->operands[in2].name, temp, len1);
+  ins->operands[in2].name[len1] = '\0';
+}
+
+
+int check_for_opcode(int * opcodes, int num_opcodes, instr_t * instr){
 
   int i = 0;
+  bool found = false;
   int opcode_num = instr_get_opcode(instr);
 
-  bool found = false;
-  for(i = 0; i < 19; i++){
+  for(; i < num_opcodes; i++){
     if(opcode_num == opcodes[i]){
-      found = true; break;
+      found = true;
+      break;
     }
   }
 
-  if(!found) return;
+  if(found) return i;
+  else return -1;
+
+}
 
 
-  char opcode[32]; int oplen = 0;
-  char src1[32]; int s1len = 0;
-  char src2[32]; int s2len = 0;
 
-  bool start1 = false;
-  bool start2 = false;
+void print_opnds(instr_t * instr){
 
-  for(i = start; i < end - 1; i++){
-    
-    if(!start1 && !start2){
-      if(buffer[i] != ' '){
-	opcode[oplen++] = buffer[i];
-      }
-      else{
-	start1 = true;
-      }
-    }
-    
+  char temp[32];
 
-    if(start1 && buffer[i] == ','){
-      start1 = false;  start2 = true;
-    }
-    else if(start1 && buffer[i] != ' '){
-      src1[s1len++] = buffer[i];
-    }
-    else if(start2 && buffer[i] != ' '){
-      src2[s2len++] = buffer[i];
-    }
+  void * drcontext = dr_get_current_drcontext();
 
-  }
-
-  int where = start;
-
-  for(i = 0; i <oplen; i++){
-    buffer[where++] = opcode[i];
-  }
-  buffer[where++] = ' ';
-  for(i = 0; i <s2len; i++){
-    buffer[where++] = src2[i];
-  }
-  buffer[where++] = ',';
-  buffer[where++] = ' ';
-  for(i = 0; i <s1len; i++){
-    buffer[where++] = src1[i];
-  }
+  int num_srcs = instr_num_srcs(instr);
+  int i = 0;
   
-
-  for(i = where; i < end - 1; i++){
-    buffer[i] = ' ';
+  dr_printf("srcs:");
+  for(i = 0; i < num_srcs; i++){
+    opnd_t op = instr_get_src(instr,i);
+    opnd_disassemble_to_buffer(drcontext, op, temp, 32);
+    dr_printf("%s,",temp);
   }
-  buffer[end - 1] = '\n';
+  dr_printf("\n");
+
+  int num_dsts = instr_num_dsts(instr);
+  i = 0;
+  
+  dr_printf("dsts:");
+  for(i = 0; i < num_dsts; i++){
+    opnd_t op = instr_get_dst(instr,i);
+    opnd_disassemble_to_buffer(drcontext, op, temp, 32);
+    dr_printf("%s,",temp);
+  }
+  dr_printf("\n");
+  
 
 
 }
 
 
-bool add_operand_size(char * buffer, unsigned start, unsigned end, instr_t * instr){
+void change_operands(ins_t * ins, instr_t * instr){
+
+  int opcode = instr_get_opcode(instr);
+
+
+  switch(opcode){
+  
+  case OP_pshufd:
+  case OP_vcvtsi2sd:
+  case OP_vmulsd:
+  case OP_vmulpd:
+  case OP_vsubsd:
+  case OP_vaddsd:
+  case OP_vdivsd:
+  case OP_vfmadd231sd:
+  case OP_vfnmadd231sd:
+  case OP_vfmadd132sd:
+  case OP_vfnmadd132sd:
+  case OP_vfmadd213sd:
+  case OP_vfnmadd213sd:
+  case OP_vextracti128:
+  case OP_vfmsub132sd:
+  case OP_imul:{
+    switch_operands(ins,0,1);
+    break;
+  }
+
+  case OP_cmpxchg:{
+    ins->num_ops = 2;
+    break;
+  }
+
+  case OP_vpinsrd:
+  case OP_vinserti128:{
+    switch_operands(ins,0,2);
+
+    if(opcode == OP_vinserti128){
+      if(ins->operands[1].name[1] == 'y'){ //cannot be a ymm
+	ins->operands[1].name[1] = 'x';
+      }
+    }
+
+    break;
+  }
+
+  case OP_vcvtdq2pd:{ //dr bug
+    if(ins->operands[0].type == REG_TYPE){
+      ins->operands[0].name[1] = 'x';
+    }
+    break;
+  }
+  
+  case OP_cmp:
+  case OP_test:
+  case OP_ptest:
+  case OP_vucomiss:
+  case OP_vucomisd:
+  case OP_vcomiss:
+  case OP_vcomisd:
+  case OP_vptest:
+  case OP_vtestps: 
+  case OP_vtestpd: 
+  case OP_bound:
+  case OP_bt:
+  case OP_ucomiss: 
+  case OP_ucomisd:
+  case OP_comiss: 
+  case OP_comisd: 
+  case OP_invept: 
+  case OP_invvpid: 
+  case OP_invpcid:{
+     DR_ASSERT(ins->num_ops >= 2);
+     switch_operands(ins, 0, 1);
+     break;
+  }
+
+  case OP_mul:
+  case OP_div:
+  case OP_idiv:{
+    ins->num_ops = 1;
+    break;
+  }
+  }
+}
+
+
+
+
+void change_opcodes(ins_t * ins,  instr_t * instr){
+  
+#undef num_opcodes 
+#define num_opcodes 2
+
+  int opcodes[num_opcodes] = {OP_cwde, OP_cdq};
+  char alt_names[num_opcodes][32] = {"cwtl", "cltd"};
+
+  int opcode = instr_get_opcode(instr);
+  
+  switch(opcode){
+  case OP_cwde:
+  case OP_cdq:{
+      
+    int index = check_for_opcode(opcodes, num_opcodes,  instr);
+    strncpy(ins->name, alt_names[index], strlen(alt_names[index]) + 1);
+    break;
+
+  }
+
+  case OP_vmovd:{
+    DR_ASSERT(instr_num_srcs(instr) == 1 && instr_num_dsts(instr) == 1);
+    opnd_t src = instr_get_src(instr,0);
+    opnd_t dst = instr_get_dst(instr,0);
+    int src_size = opnd_size_in_bytes(opnd_get_size(src));
+    int dst_size = opnd_size_in_bytes(opnd_get_size(dst));
+    int min_size = src_size > dst_size ? dst_size : src_size;
+    
+    if(min_size != 4){ //something wrong
+      char suffix = get_size_prefix(min_size);
+      if(suffix == 'e') return;
+      else ins->name[4] = suffix;      
+    }  
+    break;
+  }
+
+  case OP_vcvtsi2sd:{
+   
+    int size = opnd_size_in_bytes(opnd_get_size(instr_get_src(instr,1)));
+    char suffix = get_size_prefix(size);
+    if(suffix == 'e') return;
+    else ins->name[9] = suffix;
+    break;
+
+  }
+
+
+  }
+
+
+
+
+
+}
+
+
+void correct_movs(ins_t * ins, instr_t * instr){
+
+#undef num_opcodes
+#define num_opcodes 3
+
+  int opcodes[num_opcodes] = {OP_movsx, OP_movzx, OP_movsxd};
+
+  int index = check_for_opcode(opcodes, num_opcodes,  instr);
+  if(index == -1) return;
+
+  opnd_t opnd = instr_get_dst(instr, 0);
+  opnd_size_t size = opnd_get_size(opnd);
+  uint32_t size_in_bytes = opnd_size_in_bytes(size);
+
+  char dst_prefix = get_size_prefix(size_in_bytes);
+  
+  opnd = instr_get_src(instr, 0);
+  size = opnd_get_size(opnd);
+  size_in_bytes = opnd_size_in_bytes(size);
+
+  char src_prefix = get_size_prefix(size_in_bytes);
+  
+  if(src_prefix == 'e' || dst_prefix == 'e') return;
+
+  ins->name[4] = src_prefix;
+  ins->name[5] = dst_prefix;
+  ins->name[6] = '\0';
+
+
+}
+
+
+bool add_operand_size(ins_t * ins, instr_t * instr){
  
   //do we need to add the prefix?
   int opcode = instr_get_opcode(instr);
@@ -498,18 +679,30 @@ bool add_operand_size(char * buffer, unsigned start, unsigned end, instr_t * ins
     maxsize = maxsize < size_in_bytes ? size_in_bytes : maxsize;
   }
 
-  //if destinations are zero get it from the srcs
-  if(num_dsts == 0){
-    int num_srcs = instr_num_srcs(instr);
-    for(i = 0; i < num_srcs; i++){
-      opnd_t opnd = instr_get_src(instr,i);
-      if(opnd_is_immed(opnd)) continue;
-      opnd_size_t size = opnd_get_size(opnd);
-      uint32_t size_in_bytes = opnd_size_in_bytes(size);
+  bool immed_op = false;
+  int num_srcs = instr_num_srcs(instr);
+  for(i = 0; i < num_srcs; i++){
+    opnd_t opnd = instr_get_src(instr,i);
+    if(opnd_is_immed(opnd)){
+      immed_op = true;
+      continue;
+    } 
+    opnd_size_t size = opnd_get_size(opnd);
+    uint32_t size_in_bytes = opnd_size_in_bytes(size);
+    if(num_dsts == 0)   //if destinations are zero get it from the srcs
       maxsize = maxsize < size_in_bytes ? size_in_bytes : maxsize;
-    }
+  }
+  
+  //DR bug fixing - check for consistency later
+  if(num_dsts == 1){ //DR bug 
+    if( (opnd_is_memory_reference(instr_get_dst(instr,0))) && immed_op )
+      return false;
   }
 
+  //rep instructions are correct
+  if(strstr(ins->name,"rep")){
+    return false;
+  }
 
   char prefix = get_size_prefix(maxsize);
   
@@ -517,74 +710,14 @@ bool add_operand_size(char * buffer, unsigned start, unsigned end, instr_t * ins
     return false;
   }
 
-#define TEMP_SIZE 1024
-  char temp_buff[TEMP_SIZE];
+  //now we need to insert this letter to the end of the opcode
+  int opcode_sz = strlen(ins->name);
+  ins->name[opcode_sz] = prefix;
+  ins->name[opcode_sz + 1] = '\0';
 
-  //now we need to insert the opcode
-  //find the first space
-  for(i = start; i < end; i++){
-    if(buffer[i] == ' '){
-      if(buffer[i + 1] == ' '){//if the next character is also a space no prob!
-	buffer[i] = prefix;
-	return false;
-      }
-      else{
-	if((end - start) > TEMP_SIZE){
-	  return false;
-	}
-	if(end + 1 >= MAX_CODE_SIZE){//we cannot insert
-	  return false;
-	}
-	else{
-	  for(j = i; j < end; j++){
-	    temp_buff[j - i] = buffer[j]; 
-	  }
-	  buffer[i] = prefix;
-	  for(j = i + 1; j <= end; j++){
-	    buffer[j] = temp_buff[j - i - 1];
-	  }
-	}
-      }
-      return true;
-    }
-  }
-
-  return false;
-
-
- 
 
 }
 
-
-
-void textual_embedding(void * drcontext, code_info_t * cinfo, instrlist_t * bb){
-
-  instr_t * instr;
-  int pos = 0;
-  for(instr = instrlist_first(bb); instr != instrlist_last(bb); instr = instr_get_next(instr)){
-
-    if(filter_instr(instr)) continue;
-    
-    pos += instr_disassemble_to_buffer(drcontext,instr,cinfo->code + pos, MAX_CODE_SIZE - 1 -  pos);
-    cinfo->code[pos++] = '\n';
-    DR_ASSERT(pos <= MAX_CODE_SIZE);
-  }
-
-  cinfo->code_size = pos;
-
-}
-
-
-void print_instr(ins_t * ins){
-
-  dr_printf("%s\n",ins->name);
-  int i = 0;
-  for(; i < ins->num_ops; i++){
-    dr_printf("%s\n",ins->operands[i].name);
-  }
-
-}
 
 
 void textual_embedding_with_size(void * drcontext, code_info_t * cinfo, instrlist_t * bb){
@@ -609,15 +742,22 @@ void textual_embedding_with_size(void * drcontext, code_info_t * cinfo, instrlis
     ins_t * ins = &instrs[i];
     int length = instr_disassemble_to_buffer(drcontext, instr, disasm, 1024);
 
+    //dr_printf("dis-%s\n",disasm);
     remove_data(disasm, length);
-
-    dr_printf("%s\n",disasm);
-
     parse_instr(disasm, length, ins);
-    
-    print_instr(ins);
+    //print_instr(ins);
 
-    //dr_printf("%d\n",ins->num_ops);
+    //int opcode = instr_get_opcode(instr);
+    //if( (opcode == OP_nop) || (opcode == OP_nop_modrm) ){
+    //dr_printf("%s,%d\n",disasm, length);
+    //print_instr(ins);
+    //}
+
+    add_operand_size(ins, instr);
+    correct_movs(ins, instr);
+    change_opcodes(ins, instr);
+    change_operands(ins, instr);
+    
 
     int j = 0;
     int w = 0;
