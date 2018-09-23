1e505e563206378173b121ee6113e1f908a765b5
Charith Mendis <charithm@mit.edu>
2018-09-23 10:17:37 -0400

    restructuring data export

--- data_export/create_and_populate_db.sh
+++ data_export/create_and_populate_db.sh
@@ -1,26 +1,26 @@
 cur=$(pwd)
-cd /data/scratch/charithm/libraries/install/mysql
+cd /data/scratch/charithm/libraries/install/$1
 
- ./bin/mysql --defaults-file=my.cnf -u root -pmysql7788# -e"DROP DATABASE IF EXISTS $1; CREATE DATABASE IF NOT EXISTS $1;"
+ ./bin/mysql --defaults-file=my.cnf -u root -pmysql7788# -e"DROP DATABASE IF EXISTS $2; CREATE DATABASE IF NOT EXISTS $2;"
 
- ./bin/mysql --defaults-file=my.cnf --database=$1 -u root -pmysql7788# --max_allowed_packet=32M -f < /data/scratch/charithm/projects/cmodel/database/mysql_schema.sql
+ ./bin/mysql --defaults-file=my.cnf --database=$2 -u root -pmysql7788# --max_allowed_packet=32M -f < /data/scratch/charithm/projects/cmodel/database/$4
 
-static_files=$(find /data/scratch/charithm/projects/cmodel/data/$2 -type f -name '*.sql' -not -name 'static_perlbench_r_*' -not -name 'static_perlbench_s_*' -name 'static_*')
-dyn_files=$(find /data/scratch/charithm/projects/cmodel/data/$2 -type f -name '*.sql' -not -name 'dyn_perlbench_r_*' -not -name 'dyn_perlbench_s_*' -name 'dyn_*' )
+static_files=$(find /data/scratch/charithm/projects/cmodel/data/$3 -type f -name '*.sql' -not -name 'static_perlbench_r_*' -not -name 'static_perlbench_s_*' -name 'static_*')
+dyn_files=$(find /data/scratch/charithm/projects/cmodel/data/$3 -type f -name '*.sql' -not -name 'dyn_perlbench_r_*' -not -name 'dyn_perlbench_s_*' -name 'dyn_*' )
 
 
 for file in $static_files
 do
  # do something on $file
  echo $file
- ./bin/mysql --defaults-file=my.cnf --database=$1 -u root -pmysql7788# --max_allowed_packet=32M -f < $file
+ ./bin/mysql --defaults-file=my.cnf --database=$2 -u root -pmysql7788# --max_allowed_packet=32M -f < $file
 done
 
 for file in $dyn_files
 do
  # do something on $file
  echo $file
- ./bin/mysql --defaults-file=my.cnf --database=$1 -u root -pmysql7788# --max_allowed_packet=32M -f < $file
+ ./bin/mysql --defaults-file=my.cnf --database=$2 -u root -pmysql7788# --max_allowed_packet=32M -f < $file
 done
 
 echo $cur
