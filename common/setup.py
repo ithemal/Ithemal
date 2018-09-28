f084e1e55a82cb162e85a4c719d3cde6c3ae68da
Charith Mendis <charithm@mit.edu>
2018-09-28 16:55:52 -0400

    added a setup.py

---
+++ common/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="common_libs",
+    version="0.0.1",
+    author="Charith Mendis",
+    author_email="charithm@mit.edu",
+    description="common libs used by ithemal",
+    packages=['common_libs']
+)
