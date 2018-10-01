da1a89f677c693588c293ab555044a52c962a4af
Alex Renda <alex@renda.org>
2018-10-01 11:03:54 -0400

    more resilient dynamorio build

--- build_dynamorio.sh
+++ build_dynamorio.sh
@@ -2,7 +2,17 @@
 
 set -ex
 
-mkdir -p /home/ithemal/ithemal/data_collection/build
-cd /home/ithemal/ithemal/data_collection/build
-cmake -D -DDynamoRIO_DIR=/home/ithemal/DynamoRIO-Linux-7.0.0-RC1/cmake ..
+if [[ -z "${ITHEMAL_HOME}" ]]; then
+    echo "ITHEMAL_HOME environment variable must be set!"
+    exit 1
+fi
+
+if [[ -z "${DYNAMORIO_HOME}" ]]; then
+    echo "DYNAMORIO_HOME environment variable must be set!"
+    exit 1
+fi
+
+mkdir "${ITHEMAL_HOME}/data_collection/build"
+cd "${ITHEMAL_HOME}/data_collection/build"
+cmake -D -DDynamoRIO_DIR="${DYNAMORIO_HOME}/cmake" ..
 make -j"$(nproc --all)"
