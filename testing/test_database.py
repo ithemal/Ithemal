ff4821298a7a8b9f8dfc3ed7579a4804cbc8c172
Charith Mendis <charithm@mit.edu>
2018-10-04 09:17:25 -0700

    tests updated

--- testing/test_database.py
+++ testing/test_database.py
@@ -24,23 +24,23 @@ class TestDatabase:
         cnx = ut.create_connection_from_config('test_data/db_config.cfg')
         assert cnx != None
 
-    # def test_create_database(self,db_config):
+    def test_create_database(self,db_config):
 
-    #     create_script = os.environ['ITHEMAL_HOME'] + '/data_export/scripts/create_and_populate_db.sh'
-    #     schema = os.environ['ITHEMAL_HOME'] + '/data_export/schemas/mysql_schema.sql'
+        create_script = os.environ['ITHEMAL_HOME'] + '/data_export/scripts/create_and_populate_db.sh'
+        schema = os.environ['ITHEMAL_HOME'] + '/data_export/schemas/mysql_schema.sql'
 
-    #     proc = subprocess.call(['bash',create_script,'test_data/db_config.cfg','testIthemal',schema,'test_data'])
-    #     #_ = proc.communicate()
+        proc = subprocess.call(['bash',create_script,'test_data/db_config.cfg','testIthemal',schema,'test_data'])
+        #_ = proc.communicate()
 
-    #     cnx = ut.create_connection(user=db_config['user'],password=db_config['password'],port=db_config['port'],database='testIthemal')
-    #     assert cnx != None
+        cnx = ut.create_connection(user=db_config['user'],password=db_config['password'],port=db_config['port'],database='testIthemal')
+        assert cnx != None
 
-    #     sql = 'select count(*) from code'
+        sql = 'select count(*) from code'
 
-    #     rows = ut.execute_query(cnx, sql, True)
+        rows = ut.execute_query(cnx, sql, True)
 
-    #     assert len(rows) == 1
-    #     assert len(rows[0]) == 1
+        assert len(rows) == 1
+        assert len(rows[0]) == 1
 
-    #     assert rows[0][0] == 3287
+        assert rows[0][0] == 3287
         
