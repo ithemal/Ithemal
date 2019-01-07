f90e502a240251e0849477785db80b3c0744cc20
Alex Renda <alex@renda.org>
2019-01-07 16:17:24 -0500

    better plots

--- learning/pytorch/loss_reports/plot.py
+++ learning/pytorch/loss_reports/plot.py
@@ -7,6 +7,7 @@ import os
 import time
 from typing import List, NamedTuple
 import re
+import scipy.ndimage.filters
 
 Measurement = NamedTuple('Measurement', [
     ('label', str),
@@ -22,17 +23,19 @@ def plot_measurements(measurements):
         epochs = np.array(measurement.epochs)
         times = np.array(measurement.times)
         losses = np.array(measurement.losses)
+        losses = scipy.ndimage.filters.gaussian_filter1d(losses, 25)
         label = measurement.label
         plt.plot(times / 60, losses, label=label, color=color)
 
         ep_advance = np.where(np.diff(epochs))[0] + 1
         for idx in ep_advance:
             x = times[idx] / 60
-            plt.plot([x,x], [losses[idx] - 0.015, losses[idx] + 0.015], color=color)
+            plt.plot([x,x], [losses[idx] - 0.005, losses[idx] + 0.005], color=color)
 
     plt.title('Training loss over time')
     plt.xlabel('Time in minutes')
     plt.ylabel('Training loss (MSE / actual)')
+    plt.ylim([0, 0.4])
     plt.legend()
     plt.show()
 
