b11051a792a96f1bd9e89754a2f7f87ef3d9c9c2
Alex Renda <alex@renda.org>
2019-01-08 12:43:04 -0500

    ping after experiment complete

--- learning/pytorch/experiments/baserun.sh
+++ learning/pytorch/experiments/baserun.sh
@@ -23,3 +23,5 @@ SAVEFILE="saved/${NAMEDATE}.mdl"
 LOSS_REPORT_FILE="loss_reports/${NAMEDATE}.log"
 
 python ithemal/run_ithemal.py --embmode none --embedfile inputs/embeddings/code_delim.emb --savedatafile saved/time_skylake_1217.data --arch 1 --epochs 5 --savefile "${SAVEFILE}" --loss-report-file "${LOSS_REPORT_FILE}" "${@}"
+
+"${ITHEMAL_HOME}/aws/ping_slack.py" "Experiment ${NAME} complete"
