c82d0706e6f3c56a62f487a851b3b1e303db296b
Alex Renda <alex@renda.org>
2019-01-09 18:23:04 -0500

    start instance with IAM profile

--- aws/aws_utils/remote_setup.sh
+++ aws/aws_utils/remote_setup.sh
@@ -9,6 +9,7 @@ MYSQL_USER="$1"; shift
 MYSQL_PASSWORD="$1"; shift
 MYSQL_HOST="$1"; shift
 MYSQL_PORT="$1"; shift
+IAM_CREDENTIAL="$1"; shift
 
 sudo yum install -y docker tmux
 sudo service docker start
@@ -17,7 +18,34 @@ IMAGE_ID="654586875650.dkr.ecr.us-east-2.amazonaws.com/ithemal:latest"
 
 sudo docker login -u "${AWS_DOCKER_USER}" -p "${AWS_DOCKER_PASSWORD}" "${AWS_DOCKER_ENDPOINT}"
 sudo docker pull "${IMAGE_ID}"
-sudo docker run -dit --name ithemal -v /home/ec2-user/ithemal:/home/ithemal/ithemal -e ITHEMAL_HOME=/home/ithemal/ithemal -p 8888:8888 "${IMAGE_ID}"
+
+function dict_to_environ() {
+    PREFIX=$1; shift
+    python -c "import sys, json; print('\n'.join('${PREFIX}_{}=\'{}\''.format(*i) for i in json.load(sys.stdin).items()))"
+}
+
+# get the token secret configuration
+source <(
+    curl http://169.254.169.254/latest/meta-data/iam/security-credentials/"${IAM_CREDENTIAL}" \
+        | dict_to_environ CRED \
+        | grep 'AccessKeyId\|SecretAccessKey\|Token'
+
+    curl http://169.254.169.254/latest/dynamic/instance-identity/document \
+        | dict_to_environ REGION \
+        | grep 'region'
+)
+
+sudo docker run -dit \
+     --name ithemal \
+     -v /home/ec2-user/ithemal:/home/ithemal/ithemal \
+     -e ITHEMAL_HOME=/home/ithemal/ithemal \
+     -e AWS_ACCESS_KEY_ID="${CRED_AccessKeyId}" \
+     -e AWS_SECRET_ACCESS_KEY="${CRED_SecretAccessKey}" \
+     -e AWS_SESSION_TOKEN="${CRED_Token}" \
+     -e AWS_DEFAULT_REGION="${REGION_region}" \
+     -p 8888:8888 \
+     "${IMAGE_ID}"
+
 sudo docker exec -u ithemal ithemal bash -lc '/home/ithemal/ithemal/build_all.sh'
 sudo docker exec -d -u ithemal ithemal bash -lc "jupyter notebook --ip 0.0.0.0 --port 8888 /home/ithemal/ithemal/learning/pytorch/notebooks --no-browser --NotebookApp.token='ithemal'"
 sudo docker exec -i -u ithemal ithemal bash -lc 'cat > ~/.my.cnf' <<EOF
