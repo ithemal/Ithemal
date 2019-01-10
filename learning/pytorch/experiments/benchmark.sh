804c03ab3d5425b271212efb9d74d38bb1ccbef1
Alex Renda <alex@renda.org>
2019-01-09 23:12:42 -0500

    ping slack on completion of benchmark

--- learning/pytorch/experiments/benchmark.sh
+++ learning/pytorch/experiments/benchmark.sh
@@ -11,4 +11,12 @@ BATCH_SIZE=$1; shift
 
 NAME="benchmark_${NUM_TRAINERS}_${NUM_THREADS}_${BATCH_SIZE}"
 
-bash "${ITHEMAL_HOME}/learning/pytorch/experiments/baserun.sh" "${NAME}" --mode benchmark --trainers "${NUM_TRAINERS}" --threads "${NUM_THREADS}" --batch-size "${BATCH_SIZE}" "${@}"
+RUNTIME=$(bash "${ITHEMAL_HOME}/learning/pytorch/experiments/baserun.sh" "${NAME}" \
+               --mode benchmark \
+               --trainers "${NUM_TRAINERS}" --threads "${NUM_THREADS}" --batch-size "${BATCH_SIZE}" \
+               --savedatafile "${ITHEMAL_HOME}/learning/pytorch/inputs/data/time_skylake_test.data" \
+               "${@}" \
+              | tail -n 1 \
+              | cut -d' ' -f6
+       )
+"${ITHEMAL_HOME}/aws/ping_slack.py" "${NUM_TRAINERS} trainers, ${NUM_THREADS} threads, batch size ${BATCH_SIZE} -- ${RUNTIME} seconds"
