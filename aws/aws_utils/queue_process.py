fc5bf6d62456f601644e426ced48233152bfe0df
Alex Renda <alex@renda.org>
2019-01-09 23:10:34 -0500

    fix up queue client

--- aws/aws_utils/queue_process.py
+++ aws/aws_utils/queue_process.py
@@ -2,28 +2,32 @@
 
 import argparse
 import json
+import os
 import requests
 import subprocess
+import sys
 
-KILL_INSTANCE = 'kill'
+def kill_instance(instance_id):
+    subprocess.call(['aws', 'ec2', 'terminate-instances', '--instance-ids', instance_id])
+    sys.exit(1)
 
 def process_queue(queue_url):
     instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').text
 
     while True:
-        output = subprocess.check_output(['aws', 'sqs', 'receive-message', '--queue-url', queue_url, '--wait-time-seconds', '20'])
+        try:
+            output = subprocess.check_output(['aws', 'sqs', 'receive-message', '--queue-url', queue_url, '--wait-time-seconds', '20'])
+        except subprocess.CalledProcessError:
+            kill_instance(instance_id)
+
         if not output:
             continue
+
         messages = json.loads(output)
         message = messages['Messages'][0]
         subprocess.check_call(['aws', 'sqs', 'delete-message', '--queue-url', queue_url, '--receipt-handle', message['ReceiptHandle']])
 
-        com = message['Body']
-        if com == KILL_INSTANCE:
-            subprocess.call(['aws', 'ec2', 'terminate-instances', '--instance-ids', instance_id])
-            return
-
-        code = subprocess.call(com, shell=True)
+        code = subprocess.call(message['Body'], shell=True, cwd=os.environ['ITHEMAL_HOME'])
         if code:
             subprocess.call([os.path.join(os.environ['ITHEMAL_HOME'], 'aws', 'ping_slack.py'),
                              'Command {} failed with exit code {} on instance {}'.format(code, instance_id)])
