59a5157672df9e2a45afa7b1ad0991a0ca3831c0
Charith Mendis <charithm@mit.edu>
2019-02-17 09:07:51 -0500

    shell script to harvest elf bins

--- data_collection/disassembler/disassemble.c
+++ data_collection/disassembler/disassemble.c
@@ -3,12 +3,9 @@
 #include <assert.h>
 #include <stddef.h> /* for offsetof */
 #include <string.h> /* for memcpy */
+#include <inttypes.h>
 
-
-#define BUF_SIZE 10000
-
-unsigned char buf[BUF_SIZE];
-
+unsigned char * buf;
 
 int
 main(int argc, char *argv[])
@@ -28,39 +25,42 @@ main(int argc, char *argv[])
   ssize_t read_bytes;
   instrlist_t * bb;
   instr_t * instr;
-  byte * next_pc = buf;
+  byte * next_pc;
+  bool success;
 
-  read_bytes = dr_read_file(f, buf, BUF_SIZE);
+  uint64 filesize;
+  success = dr_file_size(f, &filesize);
+  if(success)
+    dr_printf("file size %" PRIu64 "\n",filesize);
+  else{
+    dr_printf("ERROR: cannot read file size\n");
+    exit(-1);
+  }
+    
 
+  buf = malloc(filesize);
+  next_pc = buf;
+
+  read_bytes = dr_read_file(f, buf, filesize);
   dr_printf("read %d bytes\n", read_bytes);
-  dr_printf("first byte %x\n", buf[0]);
-  disassemble(drcontext, buf, STDOUT);
 
   instr = instr_create(drcontext);
   instr_init(drcontext, instr);
 
   disassemble_set_syntax(DR_DISASM_INTEL);
 
-  while(next_pc - buf < BUF_SIZE){
+  while(next_pc - buf < filesize){
     next_pc = decode(drcontext, next_pc, instr);
+    if(!next_pc) dr_printf("invalid instruction\n"); 
     if(!next_pc) break;
     instr_disassemble(drcontext, instr, STDOUT);
-    dr_printf("\n");
+    dr_printf("-%d,\n", instr_is_cti(instr));
     instr_reset(drcontext, instr);
   }
 
-  /*bb = decode_as_bb(GLOBAL_DCONTEXT, buf);
 
-  dr_printf("printing instructions\n");
-
-  if(bb != NULL){
-    for(instr = instrlist_first(bb); instr != instrlist_last(bb) ; instr = instr_get_next(instr)){
-      instr_disassemble(drcontext, instr, STDOUT);
-    }
-    }*/
-  
-
-  //read_data(f, drcontext);
+  free(buf);
   dr_close_file(f);
+
   return 0;
 }
--- data_collection/elfs/copy_text.sh
+++ data_collection/elfs/copy_text.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-objcopy -O binary --only-section=.text $1 $2
+
+find /bin -print0 | while IFS= read -r -d '' filename
+do 
+    if file $filename | grep -q -i 'elf 64'; then
+	output=$(echo $filename | sed -e 's/.*\/\(.*\)$/\1/')
+	echo $output.o
+	objcopy -O binary --only-section=.text $filename $output.o 
+    fi
+done
