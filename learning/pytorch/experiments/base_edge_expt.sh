1b10970dd918212f0363c5a3148f63cf66ee14fa
Alex Renda <alex@renda.org>
2019-04-04 17:41:31 -0400

    untrack experiments dir

--- learning/pytorch/experiments/base_edge_expt.sh
+++
@@ -1,1 +0,0 @@
-bash "${ITHEMAL_HOME}/learning/pytorch/experiments/train.sh" --trainers 4 --threads 4 --no-residual "${@}"
--- learning/pytorch/experiments/edge/normal.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-bash "${ITHEMAL_HOME}/learning/pytorch/experiments/edge/base_edge_expt.sh" "edges_normal"
