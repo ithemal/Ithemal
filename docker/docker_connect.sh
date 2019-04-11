46cb6bb75056b534e8dcac9e647fd4a4b87c74fe
Alex Renda <alex@renda.org>
2019-04-11 15:32:00 -0400

    better docker prompt and autonotebook

--- docker/docker_connect.sh
+++ docker/docker_connect.sh
@@ -21,6 +21,9 @@ function container_id() {
     sudo docker ps -q --filter 'name=ithemal$'
 }
 
+if ! sudo -S true < /dev/null 2> /dev/null; then
+    echo "sudo access required for docker:"
+fi
 
 CONTAINER="$(container_id)"
 
