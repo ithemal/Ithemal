021973d41af991eb6a662af618bf0b7badb574bc
Alex Renda <alex@renda.org>
2019-04-17 21:02:02 -0400

    collections

--- timing_tools/llvm-mca/test_code_id.sh
+++ timing_tools/llvm-mca/test_code_id.sh
@@ -21,22 +21,24 @@ EOF
 }
 
 function to_att() {
-    ${ITHEMAL_HOME}/data_collection/build/bin/tokenizer $(cat) --att
+    ${ITHEMAL_HOME}/data_collection/disassembler/build/disassemble -att
 }
 
-if [ "$#" != "2" ]; then
-    echo "Usage: $0 arch code_id"
+
+if [ "$#" != "3" ]; then
+    echo "Usage: $0 arch kind_id code_id"
     exit 1
 fi
 
 arch=$1; shift
+kind_id=$1; shift
 code_id=$1; shift
 
-code=$(echo "SELECT code_raw FROM code WHERE code_id=${code_id}" | mysql -N | to_att | sed 's/0xf[a-fA-F0-9]+\?\([a-fA-F0-9]\{6\}\)/0xf\1/g' | sed 's/\n/\\n/g')
+code=$(echo "SELECT code_raw FROM code WHERE code_id=${code_id}" | mysql -N | to_att | tr '\t' ' ' | tr '\n' '\t' | sed 's/\t/\\n/g')
 
 if [ -z "$code" ]; then
     exit 1
 fi
 
 speed=$((template_head_att; echo -e $code; template_tail_att) | $dir/../llvm-build/bin/llvm-mca -march=x86 -mcpu $arch | grep 'Block RThroughput:' | awk '{print 100 * $3}')
-echo "INSERT INTO time (code_id, arch_id, kind_id, cycle_count) VALUES (${code_id}, 1, 3, ${speed});" | mysql
+echo "INSERT INTO time (code_id, arch_id, kind_id, cycle_count) VALUES (${code_id}, 1, ${kind_id}, ${speed});" | mysql
--- timing_tools/test_sample.sh
+++ timing_tools/test_sample.sh
@@ -2,15 +2,16 @@
 
 dir=$(dirname -- "$0")
 
-if [ "$#" != "3" ]; then
-    echo "Usage: $0 tool arch [num_to_sample]"
+if [[ "$#" -lt "3" ]]; then
+    echo "Usage: $0 tool arch kind_id [num_to_sample]"
     exit 1
 fi
 
 tool=$1; shift
 arch=$1; shift
+kind_id=$1; shift
 sample_size=${1:-99999999999}; shift
 
-echo "SELECT code_id FROM (SELECT code_id, COUNT(1) AS size FROM (SELECT t.time_id, t.code_id FROM time AS t JOIN time_metadata AS tm ON t.time_id=tm.time_id WHERE l1drmisses<=0 AND l1dwmisses<=0 AND l1imisses<=0 AND conswitch<=0) AS ts GROUP BY ts.code_id) AS q WHERE q.size > 1 ORDER BY RAND() LIMIT ${sample_size};" | \
+echo "SELECT DISTINCT(reals.code_id) FROM (SELECT code_id FROM (SELECT code_id, COUNT(1) AS size FROM (SELECT t.time_id, t.code_id FROM time AS t JOIN time_metadata AS tm ON t.time_id=tm.time_id WHERE l1drmisses<=0 AND l1dwmisses<=0 AND l1imisses<=0 AND conswitch<=0) AS ts GROUP BY ts.code_id) AS q WHERE q.size > 1) as reals LEFT JOIN (SELECT code_id FROM time WHERE kind_id=${kind_id}) as already_collected ON reals.code_id=already_collected.code_id WHERE already_collected.code_id IS NULL ORDER BY rand() LIMIT ${sample_size};" | \
     mysql -N | \
-    xargs -P $(nproc) -n 1 timeout 60s "${dir}/${tool}/test_code_id.sh" $arch
+    xargs -P 4 -n 1 timeout 60s "${dir}/${tool}/test_code_id.sh" $arch $kind_id
