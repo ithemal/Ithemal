8a89f85e98443578d86fad0d72abf746cf76af4a
Alex Renda <alex@renda.org>
2019-05-28 11:46:14 -0400

    secure the PSG slack webhook

--- aws/ping_slack.py
+++ aws/ping_slack.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 import argparse
+import base64
 import json
 import subprocess
 import urllib2
 import os
 from typing import Optional
 
-WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+# I don't really want to factor this out so pls don't use this
+WEBHOOK_URL = base64.b64decode('aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVDdTQkRNRkJSL0JGOEdNMFQ2Vy9YdldNZVN0SzRua0RoTUFHYzB5clhxVFg=')
 SLACK_USERNAME = 'AWS'
 SLACK_ICON = 'https://raw.githubusercontent.com/quintessence/slack-icons/e9e141f0a119759ca4d59e0b788fc9375c9b2678/images/amazon-web-services-slack-icon.png'
 
